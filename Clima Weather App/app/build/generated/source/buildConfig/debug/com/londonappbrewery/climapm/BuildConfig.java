aec51577b63cab5a4bf222d9a288685574a6d346
VipulGirishKumar <70092995+VipulGirishKumar@users.noreply.github.com>
2021-09-12 19:38:54 -0400

    Add files via upload

---
+++ Clima Weather App/app/build/generated/source/buildConfig/debug/com/londonappbrewery/climapm/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.londonappbrewery.climapm;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.londonappbrewery.climapm";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
